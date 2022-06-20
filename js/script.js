52dcca314c8418fbd6668c0526bd277facdcccba
Toch <tochjob@gmail.com>
2022-06-20 23:31:12 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView:'auto',
+  spaceBetween:30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  
+    
+});
